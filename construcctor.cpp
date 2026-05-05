a07a0509aadce031db71ff38971df4c778a59c7b
GordionA1 <farisazka2506@gmail.com>
2026-05-05 17:02:40 +0700

    menamabahkan defiinisi constructor dan menambahkan program utama serta objeknya

--- construcctor.cpp
+++ construcctor.cpp
@@ -5,3 +5,12 @@ class mahasiswa {
     public:
     mahasiswa(); // constructor
 };
+
+mahasiswa::mahasiswa() {
+    cout << "Constructor Terpanggil" << endl; //Definisi Constructor
+};
+
+int main() {
+    mahasiswa mhs; // pembuatan object
+    return 0;
+}
